91f80e274690761e20dabeb8fb78aff117e7f3c6
natux1 <t170d516@gunma-u.ac.jp>
2021-06-18 22:51:51 +0900

    140修正

--- Exercise140.py
+++ Exercise140.py
@@ -4,10 +4,12 @@ import scipy.stats as norm
 import matplotlib.ticker as ticker
 
 
+
 def p(N,e):
     n=N
     S=[]
     E=[]
+    ecount=0
     Ebar = []
     std = 0
     
@@ -22,7 +24,9 @@ def p(N,e):
             else:
                 n=n*10
                 E.clear()
+                ecount=ecount+1
                 continue
+    E.append(ecount)
             
     return E
             
@@ -33,12 +37,14 @@ e = 10**(-3)
 S = [[0]*100]*5
 Ebar = []
 E=[]
+ecount = 0
 fig = plt.figure()
 ax=fig.add_subplot(111,xlim=(-e,e))
 
 
 for k in range(100):
     E=p(N,e)
+    ecount = ecount + E.pop(-1)
     Ebar.append(np.average(E))
     
 
@@ -46,7 +52,11 @@ ax.set_xlabel('x')
 ax.set_ylabel('Density')
 cs=ax.plot
 ax.hist(Ebar,bins=25,rwidth=0.8)
-
-plt.show()
-    
+y_min,y_max=ax.get_ylim()
+plt.vlines(e,0,y_max,colors="red",linestyle="dashed")
+plt.vlines(-e,0,y_max,colors="red",linestyle="dashed")
+plt.vlines(0,0,y_max,colors="red")
+plt.xlim(-0.0015,0.0015)
+plt.grid(True)
+print(ecount)
 
