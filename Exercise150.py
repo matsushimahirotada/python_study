7fa7b4876895e23ba83e49f19e4c143b07fca691
natux1 <t170d516@gunma-u.ac.jp>
2021-06-19 20:28:37 +0900

    140、Nに関する修正、150作成

--- Exercise150.py
+++ Exercise150.py
@@ -4,7 +4,6 @@ import scipy.stats as norm
 import matplotlib.ticker as ticker
 
 
-
 def p(N,e):
     n=N
     S=[]
@@ -15,8 +14,12 @@ def p(N,e):
     
     while True:
         for i in range(5):
-            S=np.random.uniform(-1.0,1.0,n) 
-            E.append(sum(S)/n)
+            S=np.random.uniform(-1.0,1.0,n)
+            Sum = 0
+            temp=[]
+            for j in S:
+                temp.append(j*j)
+            E.append(sum(temp)/n)
             std = np.std(E)
         else:
             if(std<e):
@@ -24,10 +27,11 @@ def p(N,e):
             else:
                 n=n*10
                 E.clear()
+                temp.clear()
                 ecount=ecount+1
                 continue
     E.append(ecount)
-            
+    
     return E
             
             
@@ -37,8 +41,9 @@ e = 10**(-3)
 Ebar = []
 E=[]
 ecount = 0
+Ex = 1.0/3.0
 fig = plt.figure()
-ax=fig.add_subplot(111,xlim=(-e,e))
+ax=fig.add_subplot(111,xlim=(Ex-e,Ex+e))
 
 
 for k in range(100):
@@ -49,14 +54,12 @@ for k in range(100):
 
 ax.set_xlabel('x')
 ax.set_ylabel('Density')
-cs=ax.plot
 ax.hist(Ebar,bins=50,rwidth=0.8)
 y_min,y_max=ax.get_ylim()
-plt.vlines(e,0,y_max,colors="red",linestyle="dashed")
-plt.vlines(-e,0,y_max,colors="red",linestyle="dashed")
-plt.vlines(0,0,y_max,colors="red")
-plt.xlim(-0.0015,0.0015)
+plt.vlines(Ex+e,0,y_max,colors="red",linestyle="dashed")
+plt.vlines(Ex-e,0,y_max,colors="red",linestyle="dashed")
+plt.vlines(Ex,0,y_max,colors="red")
+plt.xlim(Ex-2*e,Ex+2*e)
 plt.grid(True)
 print(ecount)
-
 
